b3dab642150a8aa89eb1d971e0cf8c5fa619d95a
unknown <kamilpszczola@gmail.com>
2018-08-08 13:21:20 +0200

    Working on main site

--- js/components/MainSite/Skills.jsx
+++ js/components/MainSite/Skills.jsx
@@ -27,19 +27,26 @@ export default class Skills extends Component {
                 </p>
                 <article className='skillsItems'>
                     <img src='images/html.png' alt='html'
-                         onMouseOver={() => this.changeSkillDesc('HTML5', 'site structure with newest features of HTML5 standard')}
+                         onMouseOver={() => this.changeSkillDesc('HTML5', 'Markup language for site structuring')}
                         onMouseLeave={() => this.changeSkillDesc('','')}/>
                     <img src='images/css.png' alt='css'
-                         onMouseOver={() => this.changeSkillDesc('CSS3', 'usage of cascading styles sheets allowing for a better' +
-                             'site appearance control with newest features of CSS3 standard')}
+                         onMouseOver={() => this.changeSkillDesc('CSS3', 'Style sheet language for describing the presentation of a site')}
                          onMouseLeave={() => this.changeSkillDesc('','')}/>
                     <img src='images/js.png' alt='js'
-                         onMouseOver={() => this.changeSkillDesc('JavaScript', 'programming')}
+                         onMouseOver={() => this.changeSkillDesc('JavaScript', 'High-level programming language, core technology of the World Wide Web')}
                          onMouseLeave={() => this.changeSkillDesc('','')}/>
-                    <img src='images/react.png' alt='reactjs'/>
-                    <img src='images/webpack.png' alt='webpack'/>
-                    <img src='images/sass.png' alt='sass'/>
-                    <img src='images/jQuery.png' alt='jquery'/>
+                    <img src='images/react.png' alt='reactjs'
+                         onMouseOver={() => this.changeSkillDesc('React.js', 'JavaScript library for building user interfaces in web sites and mobile applications')}
+                         onMouseLeave={() => this.changeSkillDesc('','')}/>
+                    <img src='images/webpack.png' alt='webpack'
+                         onMouseOver={() => this.changeSkillDesc('Webpack', 'Open-source JavaScript module bundler')}
+                         onMouseLeave={() => this.changeSkillDesc('','')}/>
+                    <img src='images/sass.png' alt='sass'
+                         onMouseOver={() => this.changeSkillDesc('Sass', 'CSS extension language')}
+                         onMouseLeave={() => this.changeSkillDesc('','')}/>
+                    <img src='images/jQuery.png' alt='jquery'
+                         onMouseOver={() => this.changeSkillDesc('jQuery', 'Fast, small and feature-rich JavaScript library')}
+                         onMouseLeave={() => this.changeSkillDesc('','')}/>
                 </article>
                 <p id='specificSkill'>
                     <span>
