a9742ab7f895efc2186602f0f6c1705c5bed1174
unknown <kamilpszczola@gmail.com>
2018-08-14 11:29:26 +0200

    Added project events_manager

--- js/components/Projects.jsx
+++ js/components/Projects.jsx
@@ -9,23 +9,47 @@ export default class Projects extends Component {
             <Template activeSite="projects" scrollFunc={this.props.func}>
                 <div className='container'>
                     <SingleProject
-                        title="Testowy projekt"
-                        technologies='HTML, CSS, JavaScript'
+                        title="Events Manager"
+                        technologies='JavaScript React, Sass, Google Firebase, Google Maps/Places API'
                         projectLink='https://kamilpszczolkowski.github.io/events_manager/#/'
                         gitHubLink='https://github.com/kamilpszczolkowski/events_manager'
                         picture1='../../images/projects/1/page1.png'
                         picture2='../../images/projects/1/page2.png'
                         picture3='../../images/projects/1/page3.png'
                     >
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. A adipisci
-                        alias beatae cumque earum eveniet explicabo inventore ipsum itaque mollitia
-                        nulla, odit officia placeat praesentium quam quibusdam quis velit voluptatibus!
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. A adipisci
-                        alias beatae cumque earum eveniet explicabo inventore ipsum itaque mollitia
-                        nulla, odit officia placeat praesentium quam quibusdam quis velit voluptatibus!
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. A adipisci
-                        alias beatae cumque earum eveniet explicabo inventore ipsum itaque mollitia
-                        nulla, odit officia placeat praesentium quam quibusdam quis velit voluptatibus!
+                        <p>
+                            Application allowing users to browse through events in database and to manage them. Every
+                            event has specific data stored, which includes:
+                            <ul className='projectList'>
+                                <li>name of event</li>
+                                <li>place - name and location on the map</li>
+                                <li>start and end date</li>
+                                <li>full and short description</li>
+                                <li>type of event</li>
+                            </ul>
+                        </p>
+                        <p>
+                            Users can browse through event by two ways:
+                            <ul className='projectList'>
+                                <li>
+                                    list of events, which can be sorted by name and date of events, filtered by the
+                                    search filed.
+                                </li>
+                                <li>
+                                    by the map - every event has marker on it. When user clicks on it, the popup with
+                                    event data is shown. It can also be clicked to redirect to the event full
+                                    description site.
+                                </li>
+                            </ul>
+                        </p>
+                        <p>
+                            When user is on the specific event site, there is option to edit event - all data can be
+                            rewritten. The second option is to delete event.
+                        </p>
+                        <p>
+                            Usage of Realtime Firebase database allowed to store data in JSON format on the server. Google
+                            Map/Places api's allowed to mark places of the events on the map.
+                        </p>
                     </SingleProject>
                 </div>
             </Template>
